43c792bb423782b37a8339abe7eedc1072dccb5e
Sudhanshu Singh <sudhanshusingh@Sudhanshus-MacBook-Air.local>
2023-07-10 11:21:20 +0530

    fixed react query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import RouterConfig from './RouterConfig';
-
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import RouterConfig from "./RouterConfig";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+const queryClient = new QueryClient();
 const App: React.FC = () => {
   return (
-    <Router>
-      <RouterConfig />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <RouterConfig />
+      </Router>
+    </QueryClientProvider>
   );
 };
 
